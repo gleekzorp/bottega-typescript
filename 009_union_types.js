71802eb29a4c62a39c11b909bab07814adfb95b9
Daniel Floyd <gleek.dan@gmail.com>
2020-06-29 16:55:57 -0600

    how to use union types in typescript

---
+++ 009_union_types.js
@@ -0,0 +1,10 @@
+var players = ["Altuve", "Corerra", "Bregman"];
+var players_numbers = [25, 3, 2];
+console.log(players);
+console.log(players_numbers);
+var names;
+names = ["Daniel Floyd", "Jordan Hudgens"];
+console.log(names);
+names = "Daniel Floyd";
+console.log(names);
+//# sourceMappingURL=009_union_types.js.map
---
+++ 009_union_types.ts
@@ -0,0 +1,11 @@
+type PlayerArray = Array<string|number>;
+let players : PlayerArray = ["Altuve", "Corerra", "Bregman"];
+let players_numbers : PlayerArray = [25, 3, 2];
+console.log(players);
+console.log(players_numbers);
+
+var names : string[]|string;
+names = ["Daniel Floyd", "Jordan Hudgens"]
+console.log(names)
+names = "Daniel Floyd"
+console.log(names)
