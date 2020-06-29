351a5ee54e61073875d46c45201d93d905fcabbc
Daniel Floyd <gleek.dan@gmail.com>
2020-06-29 15:57:06 -0600

    How to Use Variables in TypeScript

---
+++ 006_variables.js
@@ -0,0 +1,26 @@
+// var fullName : string = "Daniel Floyd";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+// fullName = "Tiffany Hudgens"
+// paidAccount = false;
+// versionNumber = 1.4
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+// var
+function printNameVar(f, l) {
+    var greeting = "Hi There, ";
+    console.log(greeting + f + " " + l);
+    var greeting = "Hey There, ";
+    console.log(greeting + f + " " + l);
+}
+printNameVar("Daniel", "Floyd");
+// let
+// function printNameLet(f, l) {
+//   let greeting : string = "Hi There, ";
+//   console.log(greeting + f + " " + l)
+//   let greeting : string = "Hi There, ";
+//   console.log(greeting + f + " " + l)
+// }
+// printNameLet("Daniel", "Floyd")
+//# sourceMappingURL=006_variables.js.map
---
+++ 006_variables.ts
@@ -0,0 +1,33 @@
+// var fullName : string = "Daniel Floyd";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+
+// fullName = "Tiffany Hudgens"
+// paidAccount = false;
+// versionNumber = 1.4
+
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+
+
+// var
+function printNameVar(f, l) {
+  var greeting : string = "Hi There, ";
+  console.log(greeting + f + " " + l)
+
+  var greeting : string = "Hey There, ";
+  console.log(greeting + f + " " + l)
+}
+printNameVar("Daniel", "Floyd")
+
+
+// let
+// function printNameLet(f, l) {
+//   let greeting : string = "Hi There, ";
+//   console.log(greeting + f + " " + l)
+
+//   let greeting : string = "Hi There, ";
+//   console.log(greeting + f + " " + l)
+// }
+// printNameLet("Daniel", "Floyd")
