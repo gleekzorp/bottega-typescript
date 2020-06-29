bc9bdc4a2d4a0f178cb3aa541d1fdfc907cabf49
Daniel Floyd <gleek.dan@gmail.com>
2020-06-29 17:07:56 -0600

    Guide to TypeScript Conditionals

---
+++ 011_conditionals.js
@@ -0,0 +1,11 @@
+var password = 'asdfasdf';
+if (password == 'asdfasdf') {
+    console.log('Yes, asdfasdf is the password');
+}
+else if (password == 'zxcvzxcv') {
+    console.log('Yes, zxcvzxcv is the password');
+}
+else {
+    console.log('Sorry, permission denied');
+}
+//# sourceMappingURL=011_conditionals.js.map
