da3a85d502dbd79a91c612bea19f64e5ebc81e74
Daniel Floyd <gleek.dan@gmail.com>
2020-06-29 18:03:02 -0600

    Working with TypeScript Function Arguments

---
+++ 016_function_args.ts
@@ -0,0 +1,24 @@
+// ? is for optional
+// state is a default so it becomes optional and the type is declared as well
+// A required parameter cannot follow an optional parameter.
+// function printAddress(street: string, streetTwo?: string, state = 'AZ') {
+//   console.log(street);
+//   if (streetTwo) {
+//     console.log(streetTwo)
+//   }
+//   console.log(state)
+// }
+
+// printAddress('123 Any St')
+// printAddress('123 Any St', 'Suite 540')
+// printAddress('123 Any St', 'Suite 540', 'UT')
+
+
+function lineupCard(team: string, ...players: string[]) {
+  console.log('Team: ' + team)
+  for (let player of players) {
+    console.log(player);
+  }
+}
+
+lineupCard('Astros', 'Altuve', 'Correra', 'Bregman')
