e5f5ece1f5ecef2e0f9d86c76c64efa0b0bbfe6a
Daniel Floyd <gleek.dan@gmail.com>
2020-06-30 14:23:04 -0600

    Objects in TypeScript

---
+++ 022_objects.js
@@ -0,0 +1,17 @@
+var realUser = {
+    email: 'test@test.com',
+    firstName: 'Jordan',
+    lastName: 'Hudgens',
+    sayHi: function () {
+        return "Hey there!";
+    },
+    sayBye: function () {
+        return 'Goodbye';
+    }
+};
+console.log(realUser.email);
+console.log(realUser.firstName);
+console.log(realUser.lastName);
+console.log(realUser.sayHi());
+console.log(realUser.sayBye());
+//# sourceMappingURL=022_objects.js.map
